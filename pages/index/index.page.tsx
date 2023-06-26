71db60e83139e7cad06414a627063d9e60b3ad99
aimen08 <aimen_hamza@hotmail.com>
2023-06-26 21:20:40 +0100

    update packages

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -13,6 +13,12 @@ const documentProps = {
 const Page: Component = () => {
   return (
     <main class="px-6 py-[8vh] max-w-[76ch] mx-auto xl:text-lg dark:prose-invert">
+      <script
+        defer
+        src="https://unpkg.com/@tinybirdco/flock.js"
+        data-host="https://api.tinybird.co"
+        data-token="p.eyJ1IjogImI3ZDI0MmIwLWRmOTctNDA2ZC04Y2ZmLTAzYTAzZjlhYzFmYiIsICJpZCI6ICI4Y2VkZTYyZi04ZDMxLTQzMWQtYTJjNy0xNDhhZDNmYTE1ZTMifQ.CGsn6MmRmNfRcI3a6aC3kW80AUm7-SUrQed5_GHOzGI"
+      />
       <Hero />
       <Projects />
       <Posts />
