7b61b6c42c3c63422e8c28971083a75e612ff532
aimen08 <aimen_hamza@hotmail.com>
2023-06-26 18:41:16 +0100

    fix build

--- renderer/_default.page.client.tsx
+++ renderer/_default.page.client.tsx
@@ -1,31 +1,25 @@
-import { hydrate, render as render_ } from 'solid-js/web'
-import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client/router'
-import type { PageContext } from './types'
+import { hydrate, render as render_ } from "solid-js/web";
+import type { PageContextBuiltInClient } from "vite-plugin-ssr/client/router";
+import type { PageContext } from "./types";
 
-import 'uno.css'
-import '@unocss/reset/tailwind.css'
-import '../assets/main.css'
-import '../assets/font.css'
+import "uno.css";
+import "@unocss/reset/tailwind.css";
+import "../assets/main.css";
+import "../assets/font.css";
 
-let dispose: () => void
+let dispose: () => void;
 
 const render = async (pageContext: PageContextBuiltInClient & PageContext) => {
-  const content = document.getElementById('root')
-  const { Page, pageProps } = pageContext
+  const content = document.getElementById("root");
+  const { Page, pageProps } = pageContext;
 
-  if (dispose) dispose()
+  if (dispose) dispose();
 
   if (pageContext.isHydration) {
-    dispose = hydrate(
-      () => <Page {...pageProps} />,
-      content!,
-    )
+    dispose = hydrate(() => <Page {...pageProps} />, content!);
   } else {
-    render_(
-      () => <Page {...pageProps} />,
-      content!,
-    )
+    render_(() => <Page {...pageProps} />, content!);
   }
-}
+};
 
-export { render }
+export { render };
--- renderer/types.ts
+++ renderer/types.ts
@@ -1,13 +1,14 @@
-import { Component } from 'solid-js'
-import type { PageContextBuiltIn } from 'vite-plugin-ssr'
-export type PageProps = {}
+import { Component } from "solid-js";
+import type { PageContextBuiltIn } from "vite-plugin-ssr";
+import "vite/client";
+export type PageProps = {};
 export type PageContext = PageContextBuiltIn & {
-  Page: (pageProps: PageProps) => Component
-  pageProps: PageProps
+  Page: (pageProps: PageProps) => Component;
+  pageProps: PageProps;
   exports: {
     documentProps?: {
-      title?: string
-      description?: string
-    }
-  }
-}
+      title?: string;
+      description?: string;
+    };
+  };
+};
