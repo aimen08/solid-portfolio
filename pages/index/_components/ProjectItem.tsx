88f78bff581d49a568a4ded780614cf256e249bc
aimen08 <aimen_hamza@hotmail.com>
2024-11-23 19:06:32 +0100

    Refactor ProjectItem component and add stack icons

--- pages/index/_components/ProjectItem.tsx
+++ pages/index/_components/ProjectItem.tsx
@@ -1,15 +1,16 @@
 interface Props {
   data: {
-    name: string
-    desc: string
-    link: string
-    icon: string
-    users?: string
-  }
+    name: string;
+    desc: string;
+    link: string;
+    icon: string;
+    users?: string;
+    stack?: string[];
+  };
 }
 
 export default (props: Props) => {
-  const { data } = props
+  const { data } = props;
   return (
     <a
       class="px-4 py-3 relative rounded-md bg-gray-50 transition-colors decoration-none hover:bg-gray-100 dark:bg-gray-50/10 dark:hover:bg-gray-50/20"
@@ -21,15 +22,28 @@ export default (props: Props) => {
           <div class={data.icon} />
         </div>
         <div flex-1>
-          <div font-medium leading-relaxed>{data.name}</div>
-          <div op-50 font-normal text-sm>{data.desc}</div>
+          <div font-medium leading-relaxed>
+            {data.name}
+          </div>
+          <div op-50 font-normal text-sm>
+            {data.desc}
+          </div>
         </div>
       </div>
-      {data.users &&
-        <div class="flex justify-center items-center absolute right-2 rounded-md w-24 text-sm h-5 bg-red-400 text-white dark:bg-red-800/70">
+      {data.users && (
+        <div class="flex justify-center items-center absolute right-2 rounded-md w-20 text-[12px] h-5 bg-red-400 text-white dark:bg-red-800/70">
           {data.users}
         </div>
-      }
+      )}
+      {data.stack && (
+        <div class="flex justify-center items-center absolute right-2 -top-3 gap-1">
+          {data.stack.map((item) => (
+            <div class="flex items-center rounded-full bg-gray-50 dark:bg-gray-50/10 p-[4px]">
+              <img class="w-4 h-4" src={`/images/${item}.svg`} />
+            </div>
+          ))}
+        </div>
+      )}
     </a>
-  )
-}
+  );
+};
--- pages/index/_components/Projects.tsx
+++ pages/index/_components/Projects.tsx
@@ -8,90 +8,111 @@ export default () => {
       desc: "Develop and maintain Shopify store that offers a wide range of products.",
       link: "https://www.totencarry.com/",
       icon: "i-fluent-emoji-flat-handbag",
+      stack: ["shopify"],
     },
     {
       name: "Rusty's Off-Road",
       desc: "Maintain and develop new features for Rusty's Off-Road Shopify store.",
       link: "https://www.rustysoffroad.com/",
       icon: "i-fluent-emoji-flat-automobile",
+      stack: ["shopify"],
     },
     {
       name: "Chitchat",
       desc: "A discord bot to chat with strangers from all over the world!",
       link: "https://www.chitchat.gg/",
       icon: "i-fluent-emoji-flat-person-beard-medium-light",
+      stack: ["discord", "typescript", "docker"],
     },
     {
       name: "Wechalet ",
       desc: "WeChalet connects you with unique homes, condos and cottages for rent in nature.",
       link: "https://wechalet.com/",
       icon: "i-fluent-emoji-flat-houses",
+      stack: [
+        "react",
+        "typescript",
+        "sass",
+        "redux",
+        "reactquery",
+        "playwright",
+      ],
     },
     {
       name: "Crypto Signal and Trading Bot",
       desc: "Crypto trading bot with signals and AI and abilty to trade on Binance.",
       link: "https://t.me/Trade_Wise_bot",
       icon: "i-fluent-emoji-flat-heavy-dollar-sign",
-      users: "+4K Users"
+      users: "+4K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
     {
       name: "Voice AI Generator ",
       desc: "Telegram bot for generating Voice of famous singers using AI.",
       link: "https://t.me/AiarabBot",
       icon: "i-fluent-emoji-flat-microphone",
-      users: "+100K Users"
+      users: "+100K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
     {
       name: "QrCode AI Generator ",
       desc: "Telegram bot for generating Qr code with AI.",
       link: "https://t.me/QRcodeAIbot",
       icon: "i-fluent-emoji-flat-gem-stone",
-      users: "+8K Users"
+      users: "+8K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
     {
       name: "MediaBot",
       desc: "Telegram bot for downloading media.",
       link: "https://t.me/Media_r49_bot",
       icon: "i-fluent-emoji-flat-clapper-board",
-      users: "+120K Users"
+      users: "+120K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
     {
       name: "Phone Lookup Bot",
       desc: "Telegram bot for looking up phone numbers.",
       link: "https://t.me/number_r49_Bot",
       icon: "i-fluent-emoji-flat-mobile-phone",
-      users: "+770K Users"
+      users: "+770K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
     {
       name: "AlgeriaLogos",
       desc: "pixel perfect Algerian company logos",
       link: "https://keen-bartik-f9d7d0.netlify.app/",
       icon: "i-fluent-emoji-flat-confetti-ball",
+      stack: ["javascript", "sass"],
     },
     {
       name: "False10",
       desc: "a community website where my client shares tools and bots.",
       link: "https://www.false.ae/",
       icon: "i-fluent-emoji-flat-toolbox",
+      stack: ["nextjs", "tailwind", "typescript", "react"],
     },
     {
       name: "Pazar",
       desc: "mobile app using React Native for ecommerce",
       link: "https://play.google.com/store/apps/details?id=com.mt.pazar",
       icon: "i-fluent-emoji-flat-shopping-cart",
+      stack: ["android", "typescript", "expo"],
     },
     {
       name: "Polyglot house",
       desc: "private school website, made with NextJs",
       link: "https://polyglot-website-mcb1wnftd-aimen08.vercel.app/",
       icon: "i-fluent-emoji-flat-microscope",
+      stack: ["nextjs", "tailwind", "typescript", "react"],
     },
     {
       name: "Telegram Snapchat Bot",
       desc: "the bot fetches stories of any Snapchat user",
       link: "https://t.me/SnapChatSave_bot",
       icon: "i-fluent-emoji-flat-robot",
-      users: "+250K Users"
+      users: "+250K Users",
+      stack: ["telegram", "python", "docker", "postgresql"],
     },
   ];
   const openLink = () => {
@@ -110,7 +131,7 @@ export default () => {
           <div class="m-2 i-ri-arrow-right-up-line"></div>
         </div>
       </h2>
-      <div class="grid gap-4 grid-cols-1 sm:grid-cols-2">
+      <div class="grid gap-6 grid-cols-1 sm:grid-cols-2">
         <Index each={projectLists}>
           {(item) => <ProjectItem data={item()} />}
         </Index>
